e9498c8e862c3a8ea51f748814495c36c240e5ce
Emin Hayal <eminhayal768@gmail.com>
2021-12-19 18:40:33 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Emin Hayal on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
